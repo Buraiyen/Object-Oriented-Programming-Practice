fd720aa3bfc98f3601c5886da9b7a5436ae22334
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 12:31:23 -0800

    Testing out various human objects based on constructor

--- Prereq-OOP/src/Earth.java
+++ Prereq-OOP/src/Earth.java
@@ -1,11 +1,24 @@
 public class Earth {
 
     public static void main(String[] args) {
-        Human brian = new Human();
+        Human brian;            // Instance variable.
+        brian = new Human();    // The actual object is created when this state
         brian.name = "Brian";
         brian.age = 21;
         brian.heightInInches = 72;
         brian.eyeColor = "brown";
+
+        // Invoke method on the human object
         brian.speak();
+
+        // Declaring new human without using any parameters
+        Human tom = new Human();
+        tom.speak();
+
+        // Initialize new human with constructor parameters
+        Human chester = new Human("Chester", "brown", 41, 64);
+        chester.speak();
+
+
     }
 }
