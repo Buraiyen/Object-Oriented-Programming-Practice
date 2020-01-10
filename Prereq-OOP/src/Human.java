cfacde589596326e2852c7b4ff95515f39bd09bf
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 12:31:01 -0800

    Added new constructor

--- Prereq-OOP/src/Human.java
+++ Prereq-OOP/src/Human.java
@@ -9,13 +9,20 @@ public class Human {
     // Constructor used to construct objects
     public Human(){ }
 
+    // Constructor with passed-in values
+    public Human(String name, String eyeColor, int age, int heightInInches) {
+        this.name = name;
+        this.eyeColor = eyeColor;
+        this.age = age;
+        this.heightInInches = heightInInches;
+    }
 
     /*The methods below define how the human will behave*/
     public void speak() {
         System.out.println("Hello, my name is " + name);
         System.out.println("I am " + heightInInches + " inches tall");
         System.out.println("I am " + age + " years old");
-        System.out.println("My eye color is " + eyeColor);
+        System.out.println("My eye color is " + eyeColor + "\n");
     }
 
     public void eat() {
