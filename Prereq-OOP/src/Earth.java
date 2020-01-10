03481ce1702244982d28873b0455c1cc142a8b27
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 12:14:56 -0800

    Created driver program

---
+++ Prereq-OOP/src/Earth.java
@@ -0,0 +1,11 @@
+public class Earth {
+
+    public static void main(String[] args) {
+        Human brian = new Human();
+        brian.name = "Brian";
+        brian.age = 21;
+        brian.heightInInches = 72;
+        brian.eyeColor = "brown";
+        brian.speak();
+    }
+}
