bab8892a8570bcd61b508f0f9e7759c2c98b4641
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 13:02:30 -0800

    Added inheritance to classes

--- Prereq-OOP/src/zoo/Bird.java
+++ Prereq-OOP/src/zoo/Bird.java
@@ -1,6 +1,11 @@
 package zoo;
 
-public class Bird {
+// Bird is a subclass / child class of animal
+public class Bird extends Animal{
+    public Bird(int age, String gender, int weightInLbs) {
+        super(age, gender, weightInLbs);    // Calling the super class' constructor
+    }
+
     public void fly() {
         System.out.println("fly...");
     }
--- Prereq-OOP/src/zoo/Fish.java
+++ Prereq-OOP/src/zoo/Fish.java
@@ -1,6 +1,11 @@
 package zoo;
 
-public class Fish {
+// Bird is a subclass / child class of animal
+public class Fish extends Animal{
+    public Fish(int age, String gender, int weightInLbs) {
+        super(age, gender, weightInLbs);
+    }
+
     public void swim() {
         System.out.println("swimming...");
     }
