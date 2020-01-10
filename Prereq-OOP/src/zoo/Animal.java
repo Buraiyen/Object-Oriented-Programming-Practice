d47e6ebe70fbcc9861003fbb71201973805fd26d
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 12:50:10 -0800

    Changed some ordering of constructor

--- Prereq-OOP/src/zoo/Animal.java
+++ Prereq-OOP/src/zoo/Animal.java
@@ -5,10 +5,10 @@ public class Animal {
     int age, weightInLbs;
     String gender;
 
-    public Animal(int age, int weightInLbs, String gender) {
+    public Animal(int age, String gender, int weightInLbs) {
         this.age = age;
-        this.weightInLbs = weightInLbs;
         this.gender = gender;
+        this.weightInLbs = weightInLbs;
     }
 
     public void eat() {
