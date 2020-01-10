81ccb92f8b59678fa5ef7550dafd2831aaff96c6
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 13:03:58 -0800

    Added bird and fish objects

--- Prereq-OOP/src/zoo/Zoo.java
+++ Prereq-OOP/src/zoo/Zoo.java
@@ -5,7 +5,15 @@ public class Zoo {
     public static void main(String[] args) {
         Animal animal1 = new Animal(12, "M", 23);
         animal1.eat();
+        // animal1.fly(); (This statement will not work)
 
-        Bird bird1 = new Bird();
+        Bird bird1 = new Bird(3, "F", 5);
+        bird1.fly();
+        bird1.eat();
+
+        Fish fish1 = new Fish(10, "F", 2);
+        fish1.swim();
+        fish1.sleep();
+
     }
 }
