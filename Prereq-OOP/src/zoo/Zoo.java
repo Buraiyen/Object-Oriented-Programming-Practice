b0eab6e2dcf99556a08dab7451a6de4c08bd368e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 12:50:29 -0800

    Added test objects

--- Prereq-OOP/src/zoo/Zoo.java
+++ Prereq-OOP/src/zoo/Zoo.java
@@ -1,4 +1,11 @@
 package zoo;
 
 public class Zoo {
+
+    public static void main(String[] args) {
+        Animal animal1 = new Animal(12, "M", 23);
+        animal1.eat();
+
+        Bird bird1 = new Bird();
+    }
 }
