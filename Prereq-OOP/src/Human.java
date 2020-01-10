eabd341a1ddb5e18841a93e6e2c933b7875c406e
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 12:13:18 -0800

    Added constructor

--- Prereq-OOP/src/Human.java
+++ Prereq-OOP/src/Human.java
@@ -2,9 +2,15 @@
 // Classes contain instructions for how objects can be created as well as how objects carry out certain actions
 public class Human {
 
+    // Basic attributes for a human
     String name, eyeColor;
     int age, heightInInches;
 
+    // Constructor used to construct objects
+    public Human(){ }
+
+
+    /*The methods below define how the human will behave*/
     public void speak() {
         System.out.println("Hello, my name is " + name);
         System.out.println("I am " + heightInInches + " inches tall");
