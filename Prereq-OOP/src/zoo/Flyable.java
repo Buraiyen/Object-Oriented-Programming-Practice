33cf74552623057d4d51e4b1906cc8797635450c
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 13:35:41 -0800

    Init "Flyable" interface

---
+++ Prereq-OOP/src/zoo/Flyable.java
@@ -0,0 +1,8 @@
+package zoo;
+
+// Classes that use interfaces are compulsory to implement the methods defined in this interface
+public interface Flyable {
+
+    // Abstract methods - abstraction of the idea of something being able to fly
+    public void fly();
+}
