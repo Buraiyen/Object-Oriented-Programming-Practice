d9b1a7f1c586d1976ef1d2363eb9eca505a0d366
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-10 14:56:57 -0800

    deleted files

--- Prereq-OOP/src/zoo/Flyable.java
+++
@@ -1,8 +0,0 @@
-package zoo;
-
-// Classes that use interfaces are compulsory to implement the methods defined in this interface
-public interface Flyable {
-
-    // Abstract methods - abstraction of the idea of something being able to fly
-    public void fly();
-}
