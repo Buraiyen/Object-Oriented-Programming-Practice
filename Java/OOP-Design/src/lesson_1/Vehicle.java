f34c3d59eff5f11acd0ebf23c5241cab3505f792
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-13 15:55:07 -0800

    Added methods

--- Java/OOP-Design/src/lesson_1/Vehicle.java
+++ Java/OOP-Design/src/lesson_1/Vehicle.java
@@ -1,4 +1,14 @@
 package lesson_1;
 
 public class Vehicle {
+
+    public void startEngine(){
+        System.out.println("Starting my engine");
+    }
+    public void stopEngine(){
+        System.out.println("Stopping my engine");
+    }
+    public void accelerate(){
+        System.out.println("Accelerating...");
+    }
 }
