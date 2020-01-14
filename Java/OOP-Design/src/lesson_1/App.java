702f2de24bbbf887f9f84e9d071021419fc2c3e7
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-01-13 16:00:52 -0800

    Testing two different objects of same class

--- Java/OOP-Design/src/lesson_1/App.java
+++ Java/OOP-Design/src/lesson_1/App.java
@@ -1,4 +1,15 @@
 package lesson_1;
 
 public class App {
+
+    public static void main(String[] args) {
+
+        // Two objects are different from one another
+        Vehicle vehicle = new Vehicle();
+        Vehicle anotherVehicle = new Vehicle();
+
+        vehicle.accelerate();
+        anotherVehicle.accelerate();
+
+    }
 }
